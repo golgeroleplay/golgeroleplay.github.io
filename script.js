9aff37a492906749d8b0cc69d01ff9858238c55b
umutkaracay97-jpg <umutkaracay97@gmail.com>
2026-02-05 19:18:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -96,4 +96,25 @@ function revealOnScroll() {
 
 window.addEventListener("scroll", revealOnScroll);
 // Sayfa açıldığında da bir kere kontrol et
+
 revealOnScroll();
+/* --- Sağ Tık ve İncele Yasağı --- */
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeydown = function(e) {
+    if(event.keyCode == 123) { // F12
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { // Ctrl+Shift+I
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { // Ctrl+Shift+C
+        return false;
+    }
+    if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { // Ctrl+Shift+J
+        return false;
+    }
+    if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { // Ctrl+U
+        return false;
+    }
+}
